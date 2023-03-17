10370033f9697c6d05fa071fde70d39305e06b77
Alexey Ovsyannikov <soliterco@gmail.com>
2023-03-17 17:59:19 +0300

    add admin logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,6 +21,17 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout-success" element={<CheckoutSuccess />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<Dashboard />}>
+              <Route path="summary" element={<Summary />} />
+              <Route path="products" element={<Products />}>
+                <Route path="create-product" element={<CreateProduct />} />
+              </Route>
+              <Route path="users" element={<Users />} />
+              <Route path="orders" element={<Orders />} />
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
