e4214e3d3f6b0331216f3a1cefec1fb388f9cc36
Alexey Ovsyannikov <soliterco@gmail.com>
2023-03-17 17:51:49 +0300

    add base components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,9 @@ import NotFound from "./components/NotFound";
 import Cart from "./components/Cart";
 
 import "react-toastify/dist/ReactToastify.css";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadUser } from "./slices/authSlice";
-import CheckoutSuccess from "./components/CheckoutSuccess";
-import Dashboard from "./components/admin/Dashboard";
-import Products from "./components/admin/Products";
-import Users from "./components/admin/Users";
-import Orders from "./components/admin/Oders";
-import Summary from "./components/admin/Summary";
-import CreateProduct from "./components/admin/CreateProduct";
 
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadUser(null));
-  }, [dispatch]);
 
   return (
     <div className="App">
@@ -38,17 +21,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout-success" element={<CheckoutSuccess />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/admin" element={<Dashboard />}>
-              <Route path="summary" element={<Summary />} />
-              <Route path="products" element={<Products />}>
-                <Route path="create-product" element={<CreateProduct />} />
-              </Route>
-              <Route path="users" element={<Users />} />
-              <Route path="orders" element={<Orders />} />
-            </Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 
+import productsReducer, { productsFetch } from "./slices/productsSlice";
+import cartReducer, { getTotals } from "./slices/cartSlice";
+import authReducer from "./slices/authSlice";
+import { productsApi } from "./slices/productsApi";
+
 const store = configureStore({
   reducer: {
     products: productsReducer,
